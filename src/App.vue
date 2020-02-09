81d256bc240766b9e46c8bd4ad903fbe110606e5
unknown <987202055@qq.com>
2020-02-09 12:25:19 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    
+	<el-radio-group v-model="radio">
+		<el-radio label="线上品牌商赞助"></el-radio>
+		<el-radio label="线下场地免费"></el-radio>
+	</el-radio-group>
+	
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+    
+  },
+  data() {
+  	return {
+  		radio:""
+  	}
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style>
+
+</style>
